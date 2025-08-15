95d88c3fb0664aa4c204cad35ea482dc3734ef41
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-08-15 16:06:17 +0100

    made little changes

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ export default function LoginPage() {
     let newErrors = {};
 
     if (!formData.username.trim()) {
-      newErrors.username = "Username is required";
+      newErrors.username = "Email is required";
     }
     if (!formData.password.trim()) {
       newErrors.password = "Password is required";
@@ -42,6 +42,10 @@ export default function LoginPage() {
     console.log("Form submitted:", formData);
   };
 
+  const handlePasswordToggle = () => {
+   setShowPassword((prev) => !prev);
+  }
+
   return (
     <div className="flex flex-col md:flex-row h-screen bg-white md:overflow-hidden overflow-auto shadow-[4px_0_5px_-4px_rgba(0,0,0,0.15)] pb-13">
       {/* Left panel with animation */}
@@ -51,11 +55,6 @@ export default function LoginPage() {
         transition={{ duration: 0.6, ease: "easeInOut" }}
         className="flex items-center justify-center p-8 md:w-1/2"
       >
-        {/* <img
-          src={background}
-          alt="Eazybin Logo"
-          className="h-full w-auto max-w-full max-h-full object-contain"
-        /> */}
         <Background />
       </motion.div>
 
@@ -85,15 +84,15 @@ export default function LoginPage() {
                 htmlFor="username"
                 className="block text-sm font-medium text-gray-700 mb-1 text-left"
               >
-                Username
+                Email
               </label>
               <input
                 type="text"
                 id="username"
                 value={formData.username}
                 onChange={handleChange}
-                placeholder="Username"
-                className={`block w-full rounded-md border px-4 py-2 text-gray-900 placeholder-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm ${
+                placeholder=""
+                className={`block w-full rounded-md border px-4 py-2 text-gray-900 placeholder-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm outline-stone-300 ${
                   errors.username ? "border-red-500" : "border-gray-300"
                 }`}
               />
@@ -118,14 +117,14 @@ export default function LoginPage() {
                   id="password"
                   value={formData.password}
                   onChange={handleChange}
-                  placeholder="••••••••"
-                  className={`block w-full rounded-md border px-4 py-2 pr-10 text-gray-900 placeholder-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm ${
+                  placeholder=""
+                  className={`block w-full rounded-md border px-4 py-2 pr-10 text-gray-900 placeholder-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm outline-stone-300 ${
                     errors.password ? "border-red-500" : "border-gray-300"
                   }`}
                 />
                 <button
                   type="button"
-                  onClick={() => setShowPassword((prev) => !prev)}
+                  onClick={handlePasswordToggle}
                   className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700"
                   aria-label={showPassword ? "Hide password" : "Show password"}
                 >
