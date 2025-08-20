e6b15e549e8227af938fc3d4277e323b34f2eb02
911Dev-Niyi <anjorinadeniyi811@gmail.com>
2025-08-20 17:08:24 +0100

    Added forgot password and reset password logic

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -3,12 +3,25 @@ import {
     signupController,
     loginController,
     verifyOTPController,
+    googleAuthCallback,
+    forgotPassword,
+    resetPassword,
 } from '../controllers/authController.js';
+import passport from 'passport';
 
 const router = express.Router();
 
 router.post('/signup', signupController);
 router.post('/login', loginController);
 router.post('/verify-otp', verifyOTPController);
+
+// //To be uncommented, when OAuth is implemented
+// //Google OAuth routes
+// router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+// router.get('/google/callback', passport.authenticate('google', { session: false}), googleAuthCallback);
+
+// Password reset routes
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 export default router;
