2f71684a6f0284fc1f5b812394f7f415e63acb39
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2025-08-23 13:21:47 +0100

    added features section

---
+++ Frontend/src/components/educationalHub.jsx
@@ -0,0 +1,17 @@
+function EducationalHub() {
+  return (
+    <section className="flex flex-col items-center gap-8 bg-white shadow-2xl shadow-stone-400 px-10 py-6 rounded-2xl">
+      <h2>Educational Hub</h2>
+      <img src="./educational-icon.svg" alt="Educational-hub-icon" />
+      <div>
+        <p>How to Sort Waste properly For Disposal </p>
+        <p>How to Sort Waste properly For Disposal </p>
+        <p>How to Sort Waste properly For Disposal </p>
+        <p>How to Sort Waste properly For Disposal </p>
+      </div>
+      <button>View Hub</button>
+    </section>
+  );
+}
+
+export default EducationalHub;
--- Frontend/src/components/features.jsx
+++ Frontend/src/components/features.jsx
@@ -1,5 +1,18 @@
+import EducationalHub from "./educationalHub";
+import RedeemPoints from "./redeemPoints";
+import RequestPickUp from "./requestPickUp";
+
 function Features() {
-  return <main></main>;
+  return (
+    <main className="m-25">
+      <h3 className="text-center mb-10 text-2xl md:text-3xl font-bold">Features Overview</h3>
+      <div className="flex flex-col md:flex-row items-center gap-20 md:justify-between px-10">
+        <RequestPickUp />
+        <EducationalHub />
+        <RedeemPoints />
+      </div>
+    </main>
+  );
 }
 
 export default Features;
